72e14d580ff11067818ecb7149dfde600d004906
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-08-25 17:12:27 +0300

    chats repo added

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { UsersRepository } from './users.repository';
-import { DatabaseModule } from 'src/common/database/database.module';
+import { DatabaseModule } from '../common/database/database.module';
 import { User, UserSchema } from './entities/user.entity';
 
 @Module({
   imports: [
-    DatabaseModule.forFeature([
-      {
-        name: User.name,
-        schema: UserSchema,
-      },
-    ]),
+    DatabaseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UsersResolver, UsersService, UsersRepository],
   exports: [UsersService],
